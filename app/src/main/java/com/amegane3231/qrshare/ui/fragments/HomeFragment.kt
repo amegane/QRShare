10e7f75117b57a4e5b02b16bccb0fad594533625
amegane <amegane3231@gmail.com>
2021-05-21 14:07:44 +0900

    first commit

---
+++ app/src/main/java/com/amegane3231/qrshare/ui/fragments/HomeFragment.kt
@@ -0,0 +1,29 @@
+package com.amegane3231.qrshare.ui.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.amegane3231.qrshare.R
+import com.amegane3231.qrshare.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+    private lateinit var binding: FragmentHomeBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.buttonUpload.setOnClickListener {
+            findNavController().navigate(R.id.action_Home_to_Upload)
+        }
+    }
+}

d97b123784fd85b7f7b94ac27097f65784f8de8a
amegane <amegane3231@gmail.com>
2021-09-30 01:37:30 +0900

    Added UseCase in HomeFragment

--- app/src/main/java/com/amegane3231/qrshare/viewmodels/HomeViewModel.kt
+++ app/src/main/java/com/amegane3231/qrshare/viewmodels/HomeViewModel.kt
@@ -5,18 +5,19 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.amegane3231.qrshare.usecase.GetStorageReferenceUseCase
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.storage.StorageReference
-import com.google.firebase.storage.ktx.component1
-import com.google.firebase.storage.ktx.component2
-import com.google.firebase.storage.ktx.component3
 import com.google.firebase.storage.ktx.storage
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class HomeViewModel @Inject constructor() : ViewModel() {
+class HomeViewModel @Inject constructor(
+    private val getStorageReferenceUseCase: GetStorageReferenceUseCase
+) : ViewModel() {
     private val storage = Firebase.storage
     private val listRef = storage.reference.child("QRCode")
     private val _storageList: MutableLiveData<List<StorageReference>> by lazy {
@@ -48,14 +49,15 @@ class HomeViewModel @Inject constructor() : ViewModel() {
         }
 
         viewModelScope.launch {
-            listPageTask
-                .addOnSuccessListener { (items, prefixes, pageToken) ->
-                    this@HomeViewModel.pageToken = pageToken
+            getStorageReferenceUseCase.listAllPaginated(listPageTask).collect { task ->
+                task.addOnSuccessListener {
+                    this@HomeViewModel.pageToken = it.pageToken
                     Log.d("pageToken", pageToken.toString())
-                    _storageList.postValue(items)
+                    _storageList.postValue(it.items)
                 }.addOnFailureListener {
                     Log.e("Exception", it.toString())
                 }
+            }
         }
     }
 
--- app/src/main/java/com/amegane3231/qrshare/viewmodels/HomeViewModelFactory.kt
+++ app/src/main/java/com/amegane3231/qrshare/viewmodels/HomeViewModelFactory.kt
@@ -2,11 +2,12 @@ package com.amegane3231.qrshare.viewmodels
 
 import androidx.lifecycle.SavedStateHandle
 import com.amegane3231.qrshare.di.ViewModelFactory
+import com.amegane3231.qrshare.usecase.GetStorageReferenceUseCase
 import javax.inject.Inject
 
-class HomeViewModelFactory @Inject constructor() :
+class HomeViewModelFactory @Inject constructor(private val getStorageReferenceUseCase: GetStorageReferenceUseCase) :
     ViewModelFactory<HomeViewModel> {
     override fun create(handle: SavedStateHandle): HomeViewModel {
-        return HomeViewModel()
+        return HomeViewModel(getStorageReferenceUseCase)
     }
 }
